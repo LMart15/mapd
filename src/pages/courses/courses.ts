638d5edf8cccbe93a72c4dab3f2f725a42faa7aa
Lawrence Martin <lawrence.martin@cantire.com>
2017-04-20 20:25:26 -0400

    initial commit

---
+++ src/pages/courses/courses.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+
+@Component({
+  selector: 'page-courses',
+  templateUrl: 'courses.html',
+})
+export class CoursesPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad Courses');
+  }
+
+}
