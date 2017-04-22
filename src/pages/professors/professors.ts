d366d8dae80d7246f6c0b8cbff9c5bb043814674
Lawrence Martin <lawrence.martin@cantire.com>
2017-04-21 20:06:17 -0400

    filtering profs complete, todo comments

--- src/pages/professors/professors.ts
+++ src/pages/professors/professors.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import {AngularFire, FirebaseListObservable} from 'angularfire2';
+import {AngularFire, FirebaseObjectObservable, FirebaseListObservable} from 'angularfire2';
 import { SocialSharing } from '@ionic-native/social-sharing';
 
 
@@ -10,33 +10,74 @@ import { SocialSharing } from '@ionic-native/social-sharing';
 })
 export class ProfessorsPage {
 
-  professors: FirebaseListObservable<any>;
+  professors: FirebaseListObservable < any > ;
+  prof: FirebaseObjectObservable < any > ;
+  filteredItems: any;
+  filteredItemsCache: any;
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams, af: AngularFire, private socialSharing: SocialSharing) {
 
     this.professors = af.database.list('/professors');
+
+    af.database.list('/professors', {
+        preserveSnapshot: true
+      })
+      .subscribe(snapshots => {
+        let filteredItems = []
+        snapshots.forEach(snapshot => {
+          filteredItems.push(
+            snapshot.val()
+          );
+        });
+        this.filteredItems = filteredItems;
+        this.filteredItemsCache = filteredItems;
+      })
+
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad Professors');
+  initializeItems(): void {
+    this.filteredItems = this.filteredItemsCache;
   }
 
-onEmailProf(profEmail: string){
-    
-      this.socialSharing.canShareViaEmail().then(() => {
-        // Sharing via email is possible
-          
-      }).catch(() => {
-        // Sharing via email is not possible
-        alert("no email");
-      });
+  getFilteredItems(ev) {
 
-// Share via email
-this.socialSharing. shareViaEmail(null, null, [profEmail]).then(() => {
-  // Success!
-}).catch(() => {
-  // Error!
-});
+    this.initializeItems();
+    // use subscribe and foreach for filtering
+    var val = ev.target.value;
+
+    if (!val) {
+      return;
+    }
+
+    this.filteredItems = this.filteredItems.filter((v) => {
+      if (v.name && val) {
+        if (v.name.toLowerCase().indexOf(val.toLowerCase()) > -1) {
+          return true;
+        }
+        return false;
+      }
+    });
+  }
+
+
+  onEmailProf(profEmail: string) {
+
+    this.socialSharing.canShareViaEmail().then(() => {
+      // Sharing via email is possible
+
+    }).catch(() => {
+      // Sharing via email is not possible
+      alert("no email");
+    });
+
+    // Share via email
+    this.socialSharing.shareViaEmail(null, null, [profEmail]).then(() => {
+      // Success!
+    }).catch(() => {
+      // Error!
+    });
 
   };
   }
+
